c2b3ad1a9c73ba2962bc26cd8f84f887c255d464
Roman Hryshchyshyn <roman.hryshchyshyn@jetsoftpro.com>
2018-08-21 10:03:17 +0300

    Initial Commit

---
+++ 303TestRxSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  303TestRxSwift
+//
+//  Created by Roman on 8/21/18.
+//  Copyright © 2018 JetSoftPro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
